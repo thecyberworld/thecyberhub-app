e7e749278be49b246cd18d7dfdad0f1aa657b503
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-08-24 11:42:17 +0530

    setup

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

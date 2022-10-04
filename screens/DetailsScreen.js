36d6ee34fc9bac02114e01d9b66e24b218d11f28
ATH <alex.t.hoffmann@icloud.com>
2022-10-04 21:16:19 +0200

    update deps; style homescreen; add vid;

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
+import { Video } from 'expo-av';
 export default function DetailsScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Details Screen</Text>
-        </View>
-    );
+  return (
+    <Video
+      style={{ flex: 1 }}
+      source={{ uri: 'https://imgur.com/Q8km3ih.mp4' }}
+      shouldPlay={true}
+      isLooping={true}
+      isMuted={true}
+      resizeMode="stretch"
+    />
+  );
 }
